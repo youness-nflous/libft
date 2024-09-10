829b1a789831931eb3db50470183123b839ace80
youness-nflous <anayouness51@gmail.com>
2024-09-10 15:24:16 +0100

    atoi function

---
+++ ft_atoi.c
@@ -0,0 +1,29 @@
+
+int	ft_atoi(char *str)
+{
+	int i;
+	int sign;
+	int result;
+	
+	sign = 1;
+	i = 0;
+	result = 0;
+	while (str[i] && str[i] == ' ')
+	{
+		i++;
+	}
+	while (str[i] && (str[i] == '-' || str[i] == '+'))
+	{
+		if (str[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - 48;
+		i++;
+	}
+	return (sign * result);
+}
