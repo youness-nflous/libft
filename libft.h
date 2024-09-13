b9f6d508017417d9b13c17621f84d4feae806c9d
youness-nflous <anayouness51@gmail.com>
2024-09-13 15:45:58 +0100

    Header file

--- libft.h
+++ libft.h
@@ -1,8 +1,12 @@
 # ifndef LIBFT_H
 # define LIBFT_H
 
+#include <stdio.h>
+
 int	ft_itoa(char *str);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
+size_t	ft_strlen(const char *str);
+
 # endif
