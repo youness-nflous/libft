00e13f4d5ed4dde7c7181b04bbd7367299c3cd91
youness-nflous <anayouness51@gmail.com>
2024-09-16 21:55:12 +0100

    Header file

--- libft.h
+++ libft.h
@@ -1,7 +1,7 @@
 # ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stdio.h>
+#include <stdlib.h>
 
 int	ft_itoa(char *str);
 int	ft_isalpha(int c);
@@ -12,5 +12,6 @@ int	ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *dst, size_t n);
+void 	*ft_memcpy(void *s1, const void *s2, size_t n);
 
 # endif
