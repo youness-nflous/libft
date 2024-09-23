bcc83799a99159f716e2fdaaef2379c54959ce57
youness-nflous <anayouness51@gmail.com>
2024-09-23 23:53:39 +0100

    memcmp function

---
+++ ft_memcmp.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+int	ft_memcmp(const void *str1, const void *str2, size_t n)
+{
+	size_t i;
+	unsigned char *s1;
+	unsigned char *s2;
+	
+	i = 0;
+	s1 = (unsigned char *)str1;
+	s2 = (unsigned char *)str2;
+
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
