ef24470b61fbd64e39e4300ba5003797e1eda206
youness-nflous <anayouness51@gmail.com>
2024-09-25 15:56:43 +0100

    substr function

--- ft_substr.c
+++ ft_substr.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
+	size_t	i;
+	char	*ptr;
 	
+	if (start >= ft_strlen(str))
+	{
+		ptr = malloc(1);
+		if (ptr == NULL)
+			return (NULL);
+		ptr[0] = '\0';
+		return (ptr);
+	}
+	if (len > ft_strlen(str) - start)
+		len  = ft_strlen(str) - start;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = str[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
