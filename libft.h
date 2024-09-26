09004c18bbd36db6c51e6a4669a9b8d5a5876906
youness-nflous <anayouness51@gmail.com>
2024-09-26 12:42:33 +0100

    Header file

--- libft.h
+++ libft.h
@@ -24,6 +24,8 @@ void	*ft_memchr(const void *str, int c, size_t n);
 int	ft_memcmp(const void *str1, const void *str2, size_t n);
 char	*ft_strnstr(const char *str, const char *to_find, size_t n);
 int	ft_atoi(const char *str);
+void	*ft_calloc(size_t nitems, size_t size);
+char	*ft_strdup(const char *str);
 
 char	*ft_substr(const char *str, unsigned int start, size_t len);
 
