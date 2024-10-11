4f48740bf971ca11424b99e36c058f66d07ae21f
youness-nflous <anayouness51@gmail.com>
2024-10-11 15:15:43 +0100

    itoa function

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,62 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	count_nbr(int n)
+int	count_nbr(int nbr)
 {
 	int count;
-	
+
 	count = 0;
-
-	if (n <= 0)
+	if (nbr <= 0)
 		count = 1;
-
-	if (n < 0)
-		n = -n;
-
-	while (n != 0)
+	if (nbr < 0)
+		nbr = -nbr;
+	while (nbr != 0)
 	{
-		n = n / 10;
+		nbr = nbr / 10;
 		count++;
 	}
 	return (count);
 }
 
-
 char	*ft_itoa(int n)
 {
 	int length;
 	char *ptr;
-	int result;
-
-	result = n;
-
-	length = count_nbr(n);
+	int i;
 	
+	length = count_nbr(n);
 	ptr = malloc(sizeof(char) * (length + 1));
 	if (!ptr)
 		return (NULL);
 
-	if (result < 0)
-	{
+	ptr[length--] = '\0';
+	if (n < 0)
 		ptr[0] = '-';
-		result = -result;
-	}
-	ptr[length] = '\0';
 
+	if (n < 0)
+		n = -n; // opposite sign of n
+	i = 0;
 	while (length > 0)
 	{
-		ptr[--length] = result % 10 + '0';
-		result = result / 10;
+		ptr[length--] = n % 10 + '0';
+		n /= 10;
 	}
 	return (ptr);
 }
 
 int	main()
 {
-	int nbr = -123;
-	char *result = ft_itoa(nbr);
-
-	printf("%s", result);
-
+	printf("%s", ft_itoa(-123));
 	return (0);
 }
