b4fff90c5e55bdeb19dbe14945f6a3ce92b6c84f
youness-nflous <anayouness51@gmail.com>
2024-10-12 12:37:03 +0100

    itoa function

--- ft_itoa.c
+++ ft_itoa.c
@@ -9,7 +9,7 @@ int	count_nbr(int nbr)
 	if (nbr <= 0)
 		count = 1;
 	if (nbr < 0)
-		nbr = -nbr;
+		nbr = -nbr; // opposite sign;
 	while (nbr != 0)
 	{
 		nbr = nbr / 10;
@@ -22,30 +22,40 @@ char	*ft_itoa(int n)
 {
 	int length;
 	char *ptr;
-	int i;
-	
+	unsigned int result;
+
 	length = count_nbr(n);
 	ptr = malloc(sizeof(char) * (length + 1));
 	if (!ptr)
 		return (NULL);
-
-	ptr[length--] = '\0';
-	if (n < 0)
-		ptr[0] = '-';
-
-	if (n < 0)
-		n = -n; // opposite sign of n
-	i = 0;
-	while (length > 0)
+	
+	ptr[length] = '\0';
+	if (n == 0)
 	{
-		ptr[length--] = n % 10 + '0';
-		n /= 10;
+		ptr[0] = '0';
+		return (ptr);
+	}
+	if (n < 0)
+	{
+		ptr[0] = '-';
+		result = -n; // opposite sign;
+	}
+	else
+		result = n;
+	while (result != 0)
+	{
+		ptr[--length] = result % 10 + '0';
+		result = result / 10;
 	}
 	return (ptr);
 }
 
 int	main()
 {
-	printf("%s", ft_itoa(-123));
+	int nbr = -2147483648;
+	char *str = ft_itoa(nbr);
+
+	printf("%s", str);
+
 	return (0);
 }
